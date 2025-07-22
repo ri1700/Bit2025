71f6c2c5794ef97feb709e1ecaa652429e07802d
bit <bit@DESKTOP-HPBJEHN>
2025-07-22 11:18:36 +0900

    example

---
+++ Chapter03/src/Chapter03/ex03/GoodsApp.java
@@ -0,0 +1,21 @@
+package Chapter03.ex03;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		
+		Goods goods1 = new Goods();
+		System.out.println(Goods.countofGoods);
+		
+		Goods goods2 = new Goods();
+		System.out.println(Goods.countofGoods);
+
+		Goods goods3 = new Goods();
+		System.out.println(Goods.countofGoods);
+
+		
+		// Goods camera = new Goods();
+		
+	}
+
+}
